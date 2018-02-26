140760aff78d8e80d3733518fb8e2afd7afdcce1
Koki Ando <basket.koki.bowl@gmail.com>
2018-02-26 20:35:06 +1100

    Shiny App server code

---
+++ Desktop/Shiny app/GTD/GTD_shinyapp/server.R
@@ -0,0 +1,57 @@
+library(tidyverse)
+library(ggplot2)
+library(shiny)
+library(DT)
+library(data.table)
+library(bit64)
+library(maps)
+
+gtd <- fread("https://s3-ap-southeast-2.amazonaws.com/globalterrorismdataset/global_terrorism_dataset.csv", data.table = FALSE)
+gtd$iyear <- as.numeric(gtd$iyear)
+gtd$imonth <- as.numeric(gtd$imonth)
+gtd$iday <- as.numeric(gtd$iday)
+gtd$date <- as.Date(gtd$date)
+
+
+function(input, output){
+  
+  selected_df <- reactive({
+    req(input$select_var)
+    gtd %>% select(input$select_var)
+  })
+  
+  selected_df_period <- reactive({
+    req(input$period)
+    gtd %>% 
+      filter(iyear >= input$period[1] & iyear <= input$period[2])
+  })
+  
+  output$barplot <- renderPlot({
+    req(input$fill)
+    selected_df_period() %>% 
+      ggplot(aes_string(x=selected_df_period()$iyear, fill=input$fill)) + geom_bar() + 
+      labs(x="YEAR")
+  })
+  
+  output$map_plot <- renderPlot({
+    world %>% 
+      ggplot() + geom_map(map = world, 
+                          aes(x=long, y=lat, map_id = region),
+                          fill="white", colour = "black") + 
+      geom_point(data = 
+                   selected_df_period() %>% select(longitude, latitude) %>% na.omit(), 
+                 aes(x=longitude, y=latitude), 
+                 colour = "red", fill = "orange",
+                 alpha = 0.2, size=0.4)
+  })
+  
+  output$table <- renderDataTable({
+    DT::datatable(
+      data = selected_df(),
+      option = 
+        list(lengthMenu = c(5, 10, 50), 
+             pageLength = 5),
+      rownames = FALSE
+    )
+  })
+}
